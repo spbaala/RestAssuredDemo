36ff61856c0d3dda6c897b18c3885384bcfc787f
Bala <spbaala@gmail.com>
2021-04-16 21:23:53 +0100

    First Commit

---
+++ src/test/java/GetDemo/GetTest.java
@@ -0,0 +1,21 @@
+package GetDemo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class GetTest {
+	
+	@Test
+	public void testCityWeather()
+	{
+		Response res = RestAssured.get("https://samples.openweathermap.org/data/2.5/weather?q=London&appid=ea4282bf39c899a68b22093aaf880f90"); 
+		int code = res.getStatusCode();
+		
+		System.out.println("Status code :"+code);
+		Assert.assertEquals(code, 200);
+	}
+
+}
